938b8a489731d627030a1a4309110918b922b0bb
Konstantsin <konstantinlemesh@yandex.by>
2023-08-29 16:51:16 +0300

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let arr = str.split('');
+  let arrWithoutMinus = arr.map(function(item) {
+    if ( item !== '-') {
+        return item;
+    }
+  })
+  let reverseArr = arrWithoutMinus.reverse();
+  let newStr = reverseArr.join('');
+  return +newStr;
 }
